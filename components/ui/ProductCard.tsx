2ffc10f3065b1bf5953f46f17b7795429c757db9
AZ2411 <abizo050@gmail.com>
2024-07-19 15:17:45 +0630

    product card ui enhances

--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -14,29 +14,41 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <div className="border group cursor-pointer rounded-md flex flex-col justify-center items-center p-2 space-y-2">
-      <div className="aspect-square rounded-xl relative bg-zinc-100">
-        <Image
-          src={product?.images?.[0]?.url}
-          alt={product.name}
-          width={300}
-          height={300}
-          className="aspect-square object-fill"
+    <div className="border group cursor-pointer rounded-md flex flex-col  p-2 space-y-2 shadow-lg">
+      <div className="w-full flex items-center justify-around">
+        <p className="font-bold text-base text-gray-900">{product.name}</p>
+
+        <p className="font-medium text-sm text-gray-500"> ${product.price}</p>
+        <div
+          className="size-6 rounded-full"
+          style={{ backgroundColor: product.backcolor.value }}
         />
-        <div className="opacity-0 group-hover:opacity-100 absolute transition-all duration-300 ease-in-out w-full bottom-5 px-6">
-          <div className="flex items-center justify-center gap-x-3 ">
-            <IconButton Icon={LuExpand} onClick={() => {}} />
-            <IconButton Icon={TiShoppingCart} onClick={() => {}} />
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <div className="aspect-square rounded-xl relative bg-zinc-100">
+          <Image
+            src={product?.images?.[0]?.url}
+            alt={product.name}
+            width={250}
+            height={250}
+            className="aspect-square object-fill"
+          />
+          <div className="opacity-0 group-hover:opacity-100 absolute transition-all duration-300 ease-in-out w-full bottom-5 px-6">
+            <div className="flex items-center justify-center gap-x-3 ">
+              <IconButton Icon={LuExpand} onClick={() => {}} />
+              <IconButton Icon={TiShoppingCart} onClick={() => {}} />
+            </div>
           </div>
         </div>
       </div>
-      <div className="ml-3 md:self-start ">
+
+      {/* <div className="ml-3 md:self-start ">
         <p className="font-bold text-xl">{product.name}</p>
         <p className="font-medium text-sm text-gray-500">
           Category - {product.category.name}
         </p>
         <p className="font-bold text-lg">Price - ${product.price}</p>
-      </div>
+      </div> */}
     </div>
   );
 };
